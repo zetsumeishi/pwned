75969b1405e59872e778fd0f3c0d345c4a2e41e1
Cosmopanda <olivier.loustaunau@gmail.com>
2020-01-29 04:28:56 -0800

    Setting attributes dynamically, added HIPB class for requests

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import json
 import requests
 from time import sleep
@@ -8,6 +9,18 @@ from config import API_URL, DOMAINS, NAMES, HEADERS
 class HIPB(object):
     def __init__(self):
         super(HIPB, self).__init__()
+
+    def to_dict(self, response):
+        return json.loads(response.content)
+
+    def request(self, service=None, query=None, params=None):
+        sleep(2)
+        response = requests.get(
+            "{}/{}/{}?{}".format(API_URL, service, query, params),
+            headers=HEADERS,
+        )
+        if response:
+            return self.to_dict(response)
 
 
 class Account(object):
@@ -20,60 +33,26 @@ class Account(object):
 class Breach(object):
     def __init__(self, data):
         super(Breach, self).__init__()
-        self.name = data["Name"]
-        self.title = data["Title"]
-        self.domain = data["Domain"]
-        self.breach_date = data["BreachDate"]
-        self.added_date = data["AddedDate"]
-        self.modified_date = data["ModifiedDate"]
-        self.pwn_count = data["PwnCount"]
-        self.description = data["Description"]
-        self.logo_path = data["LogoPath"]
-        self.data_classes = data["DataClasses"]
-        self.is_verified = data["IsVerified"]
-        self.is_fabricated = data["IsFabricated"]
-        self.is_sensitive = data["IsSensitive"]
-        self.is_retired = data["IsRetired"]
-        self.is_spam_list = data["IsSpamList"]
-
-
-def display_results(results):
-    for user in results:
-        print("{}:".format(user.email))
-        for breach in user.breaches:
-            print(
-                "\tName: {}\n\t{} people pwned on {}".format(
-                    breach.title, breach.pwn_count, breach.breach_date
-                )
+        for key in data:
+            setattr(
+                self, re.sub(r"(?<!^)(?=[A-Z])", "_", key).lower(), data[key]
             )
-
-
-def to_dict(response):
-    return json.loads(response.content)
-
-
-def request(service=None, query=None, params=None):
-    sleep(2)
-    response = requests.get(
-        "{}/{}/{}?{}".format(API_URL, service, query, params), headers=HEADERS
-    )
-    if response:
-        return to_dict(response)
 
 
 def main():
     emails = ["".join(x) for x in list(product(NAMES, DOMAINS))]
     results = []
+    hipb = HIPB()
 
     for email in emails:
         account = Account(email)
-        response = request("breachedaccount", email)
+        response = hipb.request("breachedaccount", email)
         if response:
             for breach in response:
-                response = request("breach", breach["Name"])
+                response = hipb.request("breach", breach["Name"])
                 account.breaches.append(Breach(response))
         results.append(account)
-    display_results(results)
+    return results
 
 
 if __name__ == "__main__":
